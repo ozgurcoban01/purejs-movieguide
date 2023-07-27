aeb84ede3b9fa90997a088dc2d95fd08f298691f
unknown <you@example.com>
2023-07-27 17:36:30 +0300

    search compleated

--- script.js
+++ script.js
@@ -1,14 +1,83 @@
-const API_KEY="?api_key=0e0b406c41969d57b72a434bf8f8ca7a"
-const API_URL="https://api.themoviedb.org/3/discover/movie"
-let filmTitles=[];
+const banner=document.querySelector(".images")
+const navbar=document.querySelector(".myNavbar")
+const firstSearchPage=document.querySelector(".first-search-page")
+const searchedFilmPoster=document.querySelector(".searched-film-poster")
+const searchedFilms=document.querySelector(".searched-films")
+const firstSearchPageSearchContainerInput=document.querySelector(".first-search-page-search-container-input")
 
-async function  getApiData(){
-  let data= await fetch(API_URL+API_KEY).then((res)=>res.json())
-  data.results.map(movie=>filmTitles.push({name:movie.title}))
+firstSearchPageSearchContainerInput.value=""
+console.log(firstSearchPageSearchContainerInput.value)
+async function getPopularFilms(){
+    return await fetch("https://api.themoviedb.org/3/discover/movie/?api_key=0e0b406c41969d57b72a434bf8f8ca7a").then(res=>res.json())
 }
 
-getApiData();
+async function getSearchedFilms(param){
+    return await fetch("https://api.themoviedb.org/3/search/movie?api_key=0e0b406c41969d57b72a434bf8f8ca7a&query="+param).then(res=>res.json())
+}
 
-filmTitles.forEach(element => {
-    console.log(element.name)
+getPopularFilms().then(e=>e.results).then(e=>{
+
+    firstSearchPage.style.backgroundImage ="url(https://image.tmdb.org/t/p/original/"+e[0].backdrop_path+")"
+
+})
+
+/*
+getPopularFilms().then(e=>e.results).then(e=>{
+
+    firstSearchPage.style.backgroundImage ="url(https://image.tmdb.org/t/p/original/"+e[0].backdrop_path+")"
+
+    e.map(film=>{
+        searchedFilms.innerHTML+=`
+                    <div class="searched-film p-3 my-3 d-flex">
+                        <img src ="https://image.tmdb.org/t/p/original/${film.poster_path}" class="searched-film-poster rounded">
+                        <div class="searched-film-details p-3">
+                            <div class="searched-film-details-title text-white fs-1">${film.title}</div>
+                            <div class="searched-film-details-buttom d-flex">
+                                <div class="searched-film-details-year text-warning px-3 ps-0">Year: ${(film.release_date).slice(0,4)}</div>
+                                <div class="searched-film-details-score text-warning px-3">IMDB: ${film.vote_average}</div>
+                            </div>
+                        </div>
+                    </div>
+        `
+        console.log("asd")
+    })
+
+})
+*/
+
+window.addEventListener("scroll", function() {
+
+  if(window.scrollY<20){
+    navbar.style.opacity=0
+  }else{
+    navbar.style.opacity=window.scrollY/100
+  }
+  
 });
+
+firstSearchPageSearchContainerInput.addEventListener("keyup",()=>{
+    searchedFilms.innerHTML=""
+    if(firstSearchPageSearchContainerInput.value!=""){
+        searchedFilms.classList.remove("hide")
+
+        getSearchedFilms(firstSearchPageSearchContainerInput.value).then(e=>e.results).then(e=>{
+            e.map(film=>{
+                searchedFilms.innerHTML+=`
+                            <div class="searched-film p-3 my-3 d-flex">
+                                <img src ="https://image.tmdb.org/t/p/original/${film.poster_path}" class="searched-film-poster rounded">
+                                <div class="searched-film-details p-3">
+                                    <div class="searched-film-details-title text-white fs-1">${film.title}</div>
+                                    <div class="searched-film-details-buttom d-flex">
+                                        <div class="searched-film-details-year text-warning px-3 ps-0">Year: ${(film.release_date).slice(0,4)}</div>
+                                        <div class="searched-film-details-score text-warning px-3">IMDB: ${film.vote_average}</div>
+                                    </div>
+                                </div>
+                            </div>
+                `
+            })
+        })
+    }else{
+        searchedFilms.classList.add("hide")
+        searchedFilms.innerHTML=""
+    }
+})
